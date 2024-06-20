0ea1653eac1f4c0f5e7c3756ac15183c6b9122bd
Stéphanie Barbosa <100887395+Stephanie7l@users.noreply.github.com>
2024-06-20 12:21:06 -0300

    Add files via upload

---
+++ src/styles/styleVariables.js
@@ -0,0 +1,18 @@
+export const colors = {
+  background: {
+    primary: "#000",
+    secondary: "#7f260f",
+  },
+  font: {
+    primary: "#000",
+    secondary: "#ffffff",
+    title: "#ffffff",
+  },
+  modals: {
+    backgound: "#ffffff",
+  },
+};
+export const fonts = {
+  Roboto: "Roboto, regular",
+  
+};
